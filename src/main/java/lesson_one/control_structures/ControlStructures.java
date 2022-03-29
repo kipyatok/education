93ec08673bbc4cafafe103a2fb46edc69a554bc8
Sergey Ruymin <sergey.ryumin@paragon-software.com>
2022-03-29 21:47:01 +0300

    added control structures
    (cherry picked from commit f5d7c95bb5555e6c953bc99b2c1e83a96152177e)

---
+++ src/main/java/lesson_one/control_structures/ControlStructures.java
@@ -0,0 +1,43 @@
+package lesson_one.control_structures;
+
+import java.math.BigInteger;
+
+public class ControlStructures {
+
+    //todo Реализовать метод возращающий максимальное число, если числа равны вернуть -1, использовать if
+    public static int max(int a, int b) {
+        return 0;
+    }
+
+    /*todo Реализовать метод который будет возращать полное название странны
+        На вход подается сокращенное название RU - метод должен вернуть "Russia"
+        Если такой странны нет то вернуть "Not country"
+        Список стран для метода (RU, IT, DE)
+        Использовать Switch
+     */
+    public static String fullNameCountry(String country) {
+        return null;
+    }
+
+    //todo Реализовать метод который переворачить число используя цикл while
+    public static int reverse(int number) {
+        return 0;
+    }
+
+    /*todo Реализовать метод который будет считать овец
+        На входе подается число, если отрицательное вывести на экран что "Овцы так не считаются"
+        если положительное то вывод должен быть до максимума заданого числа
+        "1 овца..."
+        "2 овца..."
+        ...
+        "n овца..."
+     */
+    public static void countingSheep(int n) {
+        // You code
+    }
+
+    //todo Реализовать метод вычисления факториала, число на выходе может быть больше размерности int
+    public static BigInteger factorial(int value) {
+        return BigInteger.ONE;
+    }
+}
---
+++ src/test/java/lesson_one/control_structures/ControlStructuresTest.java
@@ -0,0 +1,68 @@
+package lesson_one.control_structures;
+
+import com.google.common.math.BigIntegerMath;
+import lesson_one.LessonOneHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ControlStructuresTest {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void testMax() {
+        assertThat(ControlStructures.max(1, 2)).isEqualTo(2);
+        assertThat(ControlStructures.max(5, 1)).isEqualTo(5);
+        assertThat(ControlStructures.max(1, 1)).isEqualTo(-1);
+    }
+
+    @Test
+    public void testFullNameCountry() {
+        assertThat(ControlStructures.fullNameCountry("RU")).isEqualTo("Russia");
+        assertThat(ControlStructures.fullNameCountry("IT")).isEqualTo("Italy");
+        assertThat(ControlStructures.fullNameCountry("DE")).isEqualTo("Germany");
+        assertThat(ControlStructures.fullNameCountry("AU")).isEqualTo("Not country");
+    }
+
+    @Test
+    public void testReverse() {
+        assertThat(ControlStructures.reverse(123)).isEqualTo(321);
+        assertThat(ControlStructures.reverse(-2932)).isEqualTo(-2392);
+        assertThat(ControlStructures.reverse(100)).isEqualTo(1);
+    }
+
+    @Test
+    public void testCountingSheep() {
+        ControlStructures.countingSheep(3);
+        assertThat(systemOutRule.getLog()).isEqualTo(LessonOneHelper.countingSheep(3));
+
+        systemOutRule.clearLog();
+
+        ControlStructures.countingSheep(50);
+        assertThat(systemOutRule.getLog()).isEqualTo(LessonOneHelper.countingSheep(50));
+
+        systemOutRule.clearLog();
+
+        ControlStructures.countingSheep(-2);
+        assertThat(systemOutRule.getLog()).isEqualTo("Овцы так не считаются\n");
+    }
+
+    @Test
+    public void testFactorial() {
+        assertThat(ControlStructures.factorial(1)).isEqualTo(1);
+        assertThat(ControlStructures.factorial(6)).isEqualTo(720);
+        assertThat(ControlStructures.factorial(100)).isEqualTo(BigIntegerMath.factorial(100));
+        assertThat(ControlStructures.factorial(666)).isEqualTo(BigIntegerMath.factorial(666));
+    }
+
+}
