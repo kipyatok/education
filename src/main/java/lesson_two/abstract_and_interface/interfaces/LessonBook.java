deb10c57f304d63470fb9adc623bd460ed8b957a
Radilushka <radilushka@gmail.com>
2022-05-05 00:17:52 +0500

    start solving problem with Book

--- src/main/java/lesson_two/abstract_and_interface/interfaces/LessonBook.java
+++ src/main/java/lesson_two/abstract_and_interface/interfaces/LessonBook.java
@@ -10,5 +10,44 @@ package lesson_two.abstract_and_interface.interfaces;
  * <p>
  * Если ничего не нашлось то возвращем для int -1, для String "";
  */
-public class LessonBook {
+public class LessonBook implements Book {
+    String book[];
+
+    @Override
+    public int sizeBook() {
+        return book.length;
+    }
+    @Override
+    public String textByPage(int page) {
+        return book[page];
+    }
+    @Override
+    public String textByRange(int start, int end) {
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            result.append(book[i]);
+        }
+        return result.toString();
+    }
+    @Override
+    public int findPageByText(String text) {
+
+        return 0;
+    }
+    @Override
+    public String text() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= book.length; i++) {
+            result.append(book[i]);
+        }
+            return result.toString();
+
+    }
+
+    LessonBook(String[] book){
+        this.book = book;
+    }
+
 }
+
+
