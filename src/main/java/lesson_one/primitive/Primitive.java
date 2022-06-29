7c7fd882c4c6dabb94ade9c6b24aeb5269c9ed78
Ignat <menshikov_92@mail.ru>
2022-06-29 19:53:52 +0300

    add method sum

--- src/main/java/lesson_one/primitive/Primitive.java
+++ src/main/java/lesson_one/primitive/Primitive.java
@@ -6,49 +6,66 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Primitive {
 
-    /** Вернуть сумму двух чисел */
+    /**
+     * Вернуть сумму двух чисел
+     */
     public static int sum(int a, int b) {
-        return 0;
+        return a + b;
     }
 
-    /** Вернуть разность двух чисел */
+    /**
+     * Вернуть разность двух чисел
+     */
     public static double sub(double a, double b) {
         return 0;
     }
 
-    /** Вернут сумму двух чисел, но вернуть надо int */
+    /**
+     * Вернут сумму двух чисел, но вернуть надо int
+     */
     public static int sumNumber(double a, double b) {
         return 0;
     }
 
-    /** Вернуть что число является четным, математическим способом */
+    /**
+     * Вернуть что число является четным, математическим способом
+     */
     public static boolean isEven(int number) {
         return false;
     }
 
-    /** Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a */
+    /**
+     * Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a
+     */
     public static char symbol(int number) {
         return '\u0000';
     }
 
-    /** Вернуть последнию цифру от числа */
+    /**
+     * Вернуть последнию цифру от числа
+     */
     public static int lastNumber(int number) {
         return 0;
     }
 
-    /** Реализовать метод возращения суммы трехзначного числа, на вход подается всегда трехзначное число */
+    /**
+     * Реализовать метод возращения суммы трехзначного числа, на вход подается всегда трехзначное число
+     */
     public static int sumExpression(int number) {
         return 0;
     }
 
-    /** Проверить что a+b=c, Допустимая погрешность – 0.0001, можно использовать Math */
+    /**
+     * Проверить что a+b=c, Допустимая погрешность – 0.0001, можно использовать Math
+     */
     public static boolean doubleExpression(double a, double b, double c) {
         return false;
     }
 
-    /** Реализовать метод, возвращающий true, если среди четырех его аргументов ровно два истинны (любые). Во всех остальных случаях метод должен возвращать false.
-        Look && || ^ !
-        Чтоб легче понять смотри так же таблицу СДНФ
+    /**
+     * Реализовать метод, возвращающий true, если среди четырех его аргументов ровно два истинны (любые). Во всех остальных случаях метод должен возвращать false.
+     * Look && || ^ !
+     * Чтоб легче понять смотри так же таблицу СДНФ
      */
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return false;
