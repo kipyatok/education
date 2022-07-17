97e968d7cf6122e1cf8c7d3563fba1d74aafb172
Ignat <menshikov_92@mail.ru>
2022-07-17 13:14:09 +0300

    Add unicode stuff ,

--- src/main/java/lesson_one/primitive/Primitive.java
+++ src/main/java/lesson_one/primitive/Primitive.java
@@ -37,38 +37,36 @@ public class Primitive {
      * Вернуть что число является четным, математическим способом
      */
     public static boolean isEven(int number) {
-
-
         return ((number) % 2) == 0 ; // odd = false , even = true
         // fixme return, remove if
         // done
-
     }
 
     /**
      * Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a
      */
     public static char symbol(int number) {
+
         char a = '\u041c';
-        int b = a;
+        char b = (int) a;
          b= (a + number);
         char c= (char)b;
-
-
-
-                        /*
-                         char a = '\u041c' ;
-                         char a = (int)a;
-                        // int b= (a + number);
-                        / char c= (char)b;
-                        */
-
         return c;
+
+/*       char A = '\u041c';
+        String hexA = String.format("%04x", (int) A); // the symbol "\" in hex
+        String hexB = Integer.toHexString( number ); // number in hex
+        int hexC;
+        hexC = (hexA + hexB);
+        char result ;
+        hexC = (char) result;
+        return result;
+         */
        // return '\u0000';
     }
 
 
-    /**
+    /*
      * Вернуть последнию цифру от числа
      */
     public static int lastNumber(int number) {
